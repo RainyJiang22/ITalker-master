488dc447f06c716cb0d332a5016d3334ebf50683
jackytallow <3434481891@qq.com>
2019-01-28 10:59:49 +0800

    编写解决Fragment的调度与重用问题，以及做了一些改动
    可以达到最优的Fragment切换

--- App/ITalker/app/src/main/java/com/example/italker_master/MainActivity.java
+++ App/ITalker/app/src/main/java/com/example/italker_master/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.italker_master;
 
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 import android.widget.TextView;
 import com.example.common.app.Activity;
+import com.example.italker_master.helper.NavHelper;
+
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+            implements BottomNavigationView.OnNavigationItemSelectedListener,
+        NavHelper.OnTabChangedListener<Integer> {
+
 
 
     @Override
     protected int getContentLayoutId() {
         return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+
+        //初始化底部辅助工具类
+
     }
 
     @Override
@@ -21,4 +37,13 @@ public class MainActivity extends Activity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public void onTabChanged(NavHelper.Tab<Integer> newTab, NavHelper.Tab<Integer> oldTab) {
+
+    }
 }
