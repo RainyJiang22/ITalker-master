250f5b0b077c816f30cb41868a60469214db8e0f
jackytallow <3434481891@qq.com>
2020-02-11 15:43:17 +0800

    开发聊天底部布局
    自定义MessageLayout作为根布局，解决顶部无法嵌入问题

--- App/ITalker-old/common/src/main/java/net/jacky/italker/common/Common.java
+++ App/ITalker-old/common/src/main/java/net/jacky/italker/common/Common.java
@@ -14,7 +14,7 @@ public class Common {
         String REGEX_MOBILE = "[1][3,4,5,7,8][0-9]{9}$";
 
         // 基础的网络请求地址
-        String API_URL = "http://192.168.1.103:9090/api/";
+        String API_URL = "http://192.168.1.106:9090/api/";
 
     }
 }
